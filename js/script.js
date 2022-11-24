50be0a9fe083f3446e3a0d3719373def0ccf3000
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-24 17:38:57 +0100

    start email exercise

--- js/script.js
+++ js/script.js
@@ -3,21 +3,42 @@
 // cercare di generare un numero casuale per entrambi i giocatori e far comparire 
 // l'alert "hai vinto o Ho vinto IO" a seconda di quale sia il numero più alto 
 
+
+
+// const button = document.getElementById("bottone");
+
+// let userRandomNumber = Math.floor(Math.random() *6 + 1);
+// console.log(userRandomNumber);
+
+// let computerRandomNumber = Math.floor(Math.random() *6 + 1);
+// console.log(computerRandomNumber);
+
+
+// if ((userRandomNumber) > (computerRandomNumber)) {
+//     alert("HAI VINTO TU!");
+// } else if ((userRandomNumber) < (computerRandomNumber)) {
+//     alert("HO VINTO IO!");
+// } else {
+//     alert("PAREGGIO");
+// }
+
+
+// ================================================================== 
+
+
+// EMAIL 
+
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+//  stampa un messaggio appropriato sull’esito del controllo.
+
+
+const emailList = [
+    "ginogino@gmail.com",
+    "minociccino@hotmail.it",
+    "luigimicco@boolean.com",
+    "stefanocappelli_pochi@boolean.com",
+    "riccardoplomooplata@boolean.uk",
+];
+
 const button = document.getElementById("bottone");
-
-let userRandomNumber = Math.floor(Math.random() *6 + 1);
-console.log(userRandomNumber);
-
-let computerRandomNumber = Math.floor(Math.random() *6 + 1);
-console.log(computerRandomNumber);
-
-
-if ((userRandomNumber) > (computerRandomNumber)) {
-    alert("HAI VINTO TU!");
-} else if ((userRandomNumber) < (computerRandomNumber)) {
-    alert("HO VINTO IO!");
-} else {
-    alert("PAREGGIO");
-}
-
 
