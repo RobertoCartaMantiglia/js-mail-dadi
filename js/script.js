13cf716d65f45968df629966a4725d523d7ea7c9
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-24 15:30:40 +0100

    write text condition

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// GIOCO DEI DADI 
+
+// cercare di generare un numero casuale per entrambi i giocatori e far comparire 
+// l'alert "hai vinto o Ho vinto IO" a seconda di quale sia il numero più alto 
+
+
+
