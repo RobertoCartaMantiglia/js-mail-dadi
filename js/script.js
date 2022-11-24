ebada788a1978aa4787e6bca87daa6928cb60730
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-24 18:27:02 +0100

    finish and add comments

--- js/script.js
+++ js/script.js
@@ -39,22 +39,33 @@ const emailList = [
     "stefanocappelli_pochi@boolean.com",
     "riccardoplomooplata@boolean.uk",
 ];
+// prima ho creato l'array (lista)
 
+// dichiaro la costante 
 const button = document.getElementById("bottone");
 
-button.addEventListener("click", function () {
+// dopo averlo fatto aggiungo l'evento click dove inizierà il mio processo di verifica dell'utente 
 
+button.addEventListener("click", function () {
+//  verifico il valore della costante del dato inserito dall'utente a seguito del click
     const userEmail = document.getElementById("verifica-email").value;
 
+// è più probabile che non vi sia nell'elenco da me fatto
     let find = false;
+
+    // per attuare la ricerca apro un ciclo for la cui ampiezza sarà pari alla lunghezza della lista. 
+    // i deve essere < di emailList.length poiché i valori in essa partono da zero e non da uno... la lista è composta da 3 valori ma il loro posizionamento parte da zero
 
     for (let i = 0; i < emailList.length; i++) {
         if (userEmail == emailList[i]) {
             find = true;
         }   
     }
+    // fuori dal for stampo la funzione find per vedere se il dato sia vero o meno
     console.log(find);
 
+
+    // se find è verificato allora parte un alert verificato
     if (find) {
         alert("Utente verificato!")
     } else {
