4824e42ae7e72f6529c098c9b00dd51c99e22be3
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-24 16:19:13 +0100

    h1 e p html

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // cercare di generare un numero casuale per entrambi i giocatori e far comparire 
 // l'alert "hai vinto o Ho vinto IO" a seconda di quale sia il numero più alto 
 
-
+const button = document.getElementById("bottone");
 
 let userRandomNumber = Math.floor(Math.random() *6 + 1);
 console.log(userRandomNumber);
